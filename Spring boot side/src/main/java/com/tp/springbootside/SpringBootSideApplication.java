1073dfea65fdd8c98987a7cbb10b1bcceb643946
yassin-elkhamlichi <yassinelkhamlichi98@gmail.com>
2026-09-24 13:24:28 +0000

    Initialize the Spring Boot Project & Angular

---
+++ Spring boot side/src/main/java/com/tp/springbootside/SpringBootSideApplication.java
@@ -0,0 +1,13 @@
+package com.tp.springbootside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSideApplication.class, args);
+    }
+
+}
---
+++ Spring boot side/src/test/java/com/tp/springbootside/SpringBootSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.springbootside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
